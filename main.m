19773f2515c52a8dd738f7d28698edef9082015a
pl <0x00.pl@gmail.com>
2024-06-26 16:45:51 +0800

    add code

--- main.m
+++ main.m
@@ -1,9 +1,10 @@
-fft_size = 512;
+fft_size = 256;
 step_size = 128;
 
 [sig, Fs] = load_data("wav_test/t_female_07_4ch.wav");
+% sig = rand(size(sig));
 sig = normalize_signal(sig);
-[ssf, ss] = stft(sig, hamming(fft_size,'periodic'), step_size, fft_size);
+[ssf, ss] = stft(sig, hann(fft_size), step_size, fft_size);
 
 % lifter signal to cpestrum domain
 figure(1); imagesc(log(abs(ssf)))
@@ -15,17 +16,34 @@ else
 end
 
 % filtering in cpestrum domain
-if 0
-    %     Num is a highpass fir
-    sc_filtered = filter(Num, 1, sc, [], 2);
-    figure(2); imagesc(abs(sc))
-    figure(3); imagesc(abs(sc_filtered))
-    sc_out = sc_filtered;
-else
-    avg_sc = sum(sc,2)/size(sc,2);
-    sc_sub = sc - avg_sc;
-    sc_out = sc_sub;
+switch(0)
+    case 0
+        %   remove average
+        avg_sc = sum(sc,2)/size(sc,2);
+        figure(10); plot(avg_sc);
+        sc_sub = sc - avg_sc;
+        sc_out = sc_sub;
+    case 1
+        % apply highpass fir in frames dimension
+        %     Num is a highpass fir
+        sc_filtered = filter(Num, 1, sc, [], 2);
+        sc_out = sc_filtered;
+    case 2
+        % apply highpass fir in qerfency dimension
+        sc_filtered = sc;
+        sc_filtered(1:3, :) = 0;
+        sc_filtered(end-1:end, :) = 0;
+        sc_out = sc_filtered;
+    case 3
+        % apply lowpass fir in qerfency dimension
+        sc_filtered = sc;
+        sc_filtered(21:end-20, :) = 0;
+        sc_out = sc_filtered;
+        
 end
+figure(2); imagesc(abs(sc))
+figure(3); imagesc(abs(sc_out))
+
 
 % reconstruct signal
 rss = invcpestrum(sc_out, fft_size, 1);
